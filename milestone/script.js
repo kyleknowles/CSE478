b9fbe0a7e7344ec2fddbdf121551257230341085
Kyle Knowles <115665968+kyleknowles@users.noreply.github.com>
2025-04-24 00:29:45 +0000

    test

--- milestone/script.js
+++ milestone/script.js
@@ -15,27 +15,30 @@ function load() {
                 });
 }
 
-const data = [
-                {id: 1, radius: 20},
-                {id: 2, radius: 25},
-                {id: 3, radius: 15},
-                {id: 4, radius: 30},
-                {id: 5, radius: 18},
-            ];
+function load2() {
+    const data = [
+        {id: 1, radius: 20},
+        {id: 2, radius: 25},
+        {id: 3, radius: 15},
+        {id: 4, radius: 30},
+        {id: 5, radius: 18},
+    ];
 
 const svg = d3.select("svg");
 
 const circles = svg.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", (d, i) => 60 + i * 80)
-    .attr("cy", 60)
-    .attr("r", d => d.radius)
-    .attr("fill", "steelblue")
-    .on("click", function(event, d) {
-        d3.select(this).classed("highlighted", function() {
-            return !d3.select(this).classed("highlighted");
-        });
-    });
+.data(data)
+.enter()
+.append("circle")
+.attr("cx", (d, i) => 60 + i * 80)
+.attr("cy", 60)
+.attr("r", d => d.radius)
+.attr("fill", "steelblue")
+.on("click", function(event, d) {
+d3.select(this).classed("highlighted", function() {
+    return !d3.select(this).classed("highlighted");
+});
+});
 alert("F this")
+
+}
