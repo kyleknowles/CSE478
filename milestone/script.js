d3f472a1273ee7fe3a98bfb2e60451949e621613
Kyle Knowles <115665968+kyleknowles@users.noreply.github.com>
2025-04-25 21:26:16 +0000

    ?

--- milestone/script.js
+++ milestone/script.js
@@ -46,7 +46,7 @@
                     .enter()
                     .append("rect")
                     .attr("x", (d, i) => i * (barWidth + (barWidth/2)))
-                    .attr("y", d => d["Height (inches)"])
+                    .attr("y", d => 100)
                     .attr("width", barWidth)
                     .attr("height", d => d["Height (inches)"] * 3)
                     .attr("fill", "steelblue")
