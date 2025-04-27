082cae33cd98bf786382daa43aa797863106cec1
Kyle Knowles <115665968+kyleknowles@users.noreply.github.com>
2025-04-27 04:00:45 +0000

    ?

--- assignment2/script.js
+++ assignment2/script.js
@@ -25,7 +25,7 @@ fetch("data.csv")
         
         /*
         const chart = svg.append("g")
-            .attr("transform", `translate(${margin.left}, ${margin.top})`);
+            .attr("transform", `translate(${2}, ${2})`);
 */
         const maxValue = Math.max(...data.map(d => d.value)); 
 
