17bcb2b047c64cfbc4eb3cd38dc312cdd7f481e0
Kyle Knowles <115665968+kyleknowles@users.noreply.github.com>
2025-04-28 20:38:43 +0000

    ?

--- assignment2/script.js
+++ assignment2/script.js
@@ -49,7 +49,7 @@ fetch("data.csv")
             .padding(0.1);
 
         const yScale = d3.scaleLinear()
-            .domain([0, d3.max(data, d => d.value) * 2.05])
+            .domain([0, 20])
             .range([0, height]);
 
 
