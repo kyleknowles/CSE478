0cc274b1adc0fdaf3a92e0e041d270d9769ae2b4
Kyle Knowles <115665968+kyleknowles@users.noreply.github.com>
2025-04-28 20:28:09 +0000

    ?

--- assignment2/script.js
+++ assignment2/script.js
@@ -89,7 +89,7 @@ fetch("data.csv")
             .append("text")
 
             .attr("x", d => xScale(d.name) + xScale.bandwidth() / 2)
-            .attr("y", d => 450 + margin.bottom - yScale(d.value) - 3) // + 6 * d.name.length
+            .attr("y", d => 450 - margin.bottom - yScale(d.value) - 3)
             .attr("text-anchor", "middle")
             
             //.text(d => d.name);
